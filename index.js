863e5e6ba3d14dd96f2d5208188bb0c669a62ed9
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-03-11 09:15:18 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {name: "Jason", 
+                  streetAddress: "6774, 133st. Surrey, BC."};
+function updateEmployeeWithKeyAndValue (employee, name, streetAddress)  
+{
+    const newEmployee = { ...employee};
+     newEmployee [name] = streetAddress;
+     return newEmployee;
+}   
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, name, streetAddress)
+{
+    employee [name] = streetAddress;
+    return employee;
+}
+function deleteFromEmployeeByKey (employee, name, streetAddress)
+{
+    const newEmployee = { ...employee};
+    delete newEmployee.name;
+    return newEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey (employee, name, streetAddress){
+    delete employee.name;
+    return employee;
+}
